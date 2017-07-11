4e5033bd13f247d583d2c7c5bdc03f38f649d912
grahamimac <graham3333@yahoo.com>
2017-07-11 15:44:30 -0400

    Update site to include data and descriptions

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,11 @@
 // The JS
+
+states = {"Alabama":{"url":"01","size":23.2},"Alaska":{"url":"02","size":0.4},"Arizona":{"url":"04","size":10.6},"Arkansas":{"url":"05","size":8.1},"California":{"url":"06","size":155.9},"Colorado":{"url":"08","size":7.2},"Connecticut":{"url":"09","size":58.4},"Delaware":{"url":"10","size":17.7},"District Of Columbia":{"url":"11","size":9.4},"Florida":{"url":"12","size":62.6},"Georgia":{"url":"13","size":44.7},"Hawaii":{"url":"15","size":0.4},"Idaho":{"url":"16","size":0.8},"Illinois":{"url":"17","size":96.1},"Indiana":{"url":"18","size":60},"Iowa":{"url":"19","size":12.1},"Kansas":{"url":"20","size":8.8},"Kentucky":{"url":"21","size":30.7},"Louisiana":{"url":"22","size":12},"Maine":{"url":"23","size":4.5},"Maryland":{"url":"24","size":79.8},"Massachusetts":{"url":"25","size":53.6},"Michigan":{"url":"26","size":83.5},"Minnesota":{"url":"27","size":11.6},"Mississippi":{"url":"28","size":9},"Missouri":{"url":"29","size":18.2},"Montana":{"url":"30","size":0.3},"Nebraska":{"url":"31","size":4.4},"Nevada":{"url":"32","size":5.4},"New Hampshire":{"url":"33","size":9.4},"New Jersey":{"url":"34","size":144.4},"New Mexico":{"url":"35","size":1.4},"New York":{"url":"36","size":254.1},"North Carolina":{"url":"37","size":51.4},"North Dakota":{"url":"38","size":0.3},"Ohio":{"url":"39","size":103.6},"Oklahoma":{"url":"40","size":15.3},"Oregon":{"url":"41","size":7.6},"Pennsylvania":{"url":"42","size":189.5},"Rhode Island":{"url":"44","size":12.7},"South Carolina":{"url":"45","size":26.9},"South Dakota":{"url":"46","size":0.8},"Tennessee":{"url":"47","size":33.2},"Texas":{"url":"48","size":65.2},"Utah":{"url":"49","size":1.9},"Vermont":{"url":"50","size":6},"Virginia":{"url":"51","size":69.7},"Washington":{"url":"53","size":12.1},"West Virginia":{"url":"54","size":16.3},"Wisconsin":{"url":"55","size":35.7},"Wyoming":{"url":"56","size":0.6}}
+
+$(document).ready(function(){
+
+	for (key in states){
+		$('#fillList').append('<li><a href="https://s3.amazonaws.com/ui-twitter/Misc_Helper_Files/tract_data/tract_drive_times_' + states[key]["url"] + '.zip">' + key + '</a> (' + states[key]["size"] + ' MB)</li>')
+	}	
+
+})
